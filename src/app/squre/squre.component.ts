543022a33d62734ee0d8fb4bec0599ecdede347e
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-01-31 08:38:18 +0530

    components initialized

---
+++ src/app/squre/squre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-squre',
+  templateUrl: './squre.component.html',
+  styleUrls: ['./squre.component.scss']
+})
+export class SqureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
