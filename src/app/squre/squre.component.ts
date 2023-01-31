80d61d0beaf3b64cb79b1f15b1150f65ce4879e5
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-01-31 08:40:14 +0530

    square component done

--- src/app/squre/squre.component.ts
+++ src/app/squre/squre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-squre',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SqureComponent implements OnInit {
 
+  @Input() value !:'X' | 'O'
   constructor() { }
 
   ngOnInit(): void {
